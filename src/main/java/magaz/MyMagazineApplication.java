6fa2c0d62ab65222b0692a7ba1b7e2b0d1f4eeca
VelazqueZzz <93205261+VelazqueZzz@users.noreply.github.com>
2024-12-09 23:40:02 +0300

    Add files via upload

---
+++ src/main/java/magaz/MyMagazineApplication.java
@@ -0,0 +1,13 @@
+package magaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMagazineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMagazineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/magaz/MyMagazineApplicationTests.java
@@ -0,0 +1,13 @@
+package magaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMagazineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
